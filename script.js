5ef3269c4e6cafbc189779b311f584aa82a7db0d
Khyati Kumari <108214217+Khyatikumari@users.noreply.github.com>
2024-09-24 21:24:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        document.querySelector(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
